309807b7d133e77e1ac9b6830b99b3ca865d971d
Hatem Ramadan <hramadan@sumerge.com>
2019-12-29 16:29:17 +0200

    Fix: fixed overfetching propblem by combining action createors #Blog App

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,21 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 export const fetchPosts = () => {
-  return async function(dispatch, getState) {
+  return async dispatch => {
     const response = await jsonPlaceHolder.get("/posts");
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
+  };
+};
+
+export const fetchUser = id => {
+  return async dispatch => {
+    const response = await jsonPlaceHolder.get("/users/" + id);
+    dispatch({ type: "FETCH_USER", payload: response.data });
   };
 };
